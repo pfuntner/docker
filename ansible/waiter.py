0d05ce17a51b9c5783da5e1c5ad7a06685877318
John Pfuntner <jpfuntne@cisco.com>
2018-12-07 12:52:16 -0500

    Improving things a little

--- ansible/waiter.py
+++ ansible/waiter.py
@@ -5,10 +5,10 @@ import time
 import datetime
 
 pgm = sys.argv[0]
-now = datetime.datetime.now()
-msg = '{pgm} started at {now}'.format(**locals())
-print msg
-with open('waiter.log', 'w') as stream:
-  stream.write('{msg}\n'.format(**locals()))
-
-time.sleep(2**32)
+with open('/tmp/waiter.log', 'w') as stream:
+  while True:
+    now = datetime.datetime.now()
+    msg = '{pgm} running at {now}'.format(**locals())
+    stream.write('{msg}\n'.format(**locals()))
+    print msg
+    time.sleep(60**2)
