c780b02a0e4913ebe7f3715d998734b8f1d15a1d
John Pfuntner <jpfuntne@cisco.com>
2018-12-11 11:15:40 -0500

    Enabling sudo
    I installed sudo on all the containers and enabled the non-privledged user to use sudo without a password.

--- ansible/setup.py
+++ ansible/setup.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import os
+import re
 import sys
 import pwd
 import json
@@ -47,6 +48,20 @@ for filename in ['id_rsa', 'id_rsa.pub', 'authorized_keys']:
   run('cp -v /root/.ssh/{filename} /home/peon/.ssh/'.format(**locals()))
 run('chown -Rv peon /home/peon/.ssh')
 run('chmod -Rv 700 /home/peon/.ssh')
+
+print 'Granting peon the ability to use sudo'
+run('usermod -a -G sudo peon')
+
+print 'Enabling peon to use sudo without a password'
+regexp = re.compile(r'^%sudo\s+ALL=\(ALL:ALL\)\s+ALL')
+with open('/etc/sudoers.new', 'w') as newfile:
+  with open('/etc/sudoers', 'r') as oldfile:
+    for line in oldfile:
+      if regexp.search(line):
+        line = '%sudo\tALL=(ALL:ALL) NOPASSWD: ALL\n'
+      newfile.write(line)
+run('mv -v /etc/sudoers /etc/sudoers.old')
+run('mv -v /etc/sudoers.new /etc/sudoers')
 
 peon = pwd.getpwnam('peon').pw_uid
 print 'peon uid is {peon}'.format(**locals())
